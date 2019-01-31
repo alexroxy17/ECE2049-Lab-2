edf7adee978ec6ec05583979cd1c463e0c9cded7
Nate <nsbajakian@wpi.edu>
2019-01-31 16:37:19 -0500

    Finished Song of Storms

--- main.c
+++ main.c
@@ -50,7 +50,7 @@ void main(void)
                               {NOTE_F5,  2, 0}, //10
                               //E4 D4 A4
                               {NOTE_E5,  2, 0},
-                              {NOTE_D5,  2, 0},
+                              {NOTE_C5,  2, 0}, //Corrected from D5
                               {NOTE_A4,  7, 0},
                               {REST, 1, 0},
 
@@ -83,7 +83,7 @@ void main(void)
                               {NOTE_F5,  2, 0}, //35
                               //E4 D4 A4
                               {NOTE_E5,  2, 0},
-                              {NOTE_D5,  2, 0},
+                              {NOTE_C5,  2, 0},
                               {NOTE_A4,  7, 0},
                               {REST, 1, 0},
 
@@ -92,15 +92,104 @@ void main(void)
                               {NOTE_F4,  2, 0},
                               {NOTE_G4,  2, 0},
 
-                              {NOTE_A4,  9, 0},
+                              {NOTE_A4,  7, 0},
                               {REST, 1, 0},     //45
 
                               {NOTE_A4,  4, 0},
+                              {NOTE_D3,  4, 0},
+                              {NOTE_D4,  3, 0},
+                              {REST, 1, 0},
                               {NOTE_D4,  4, 0},
-                              {NOTE_F4,  2, 0},
-                              {NOTE_G4,  2, 0},
 
-                              {NOTE_F4,  10, 0} //50
+                              {REST, 2, 0},
+                              {NOTE_E3, 2, 0},
+                              {NOTE_E4, 4, 0},
+
+                              {REST, 4, 0},
+                              {NOTE_F4, 4, 0},
+                              {NOTE_F4, 4, 0},
+
+                              {REST, 2, 0},
+                              {NOTE_E3, 2, 0},
+                              {NOTE_E4, 4, 0},
+
+                              {REST, 4, 0},
+                              {NOTE_D4, 4, 0},
+                              {NOTE_D4, 4, 0},
+
+                              {REST, 2, 0},
+                              {NOTE_E4, 2, 0},
+                              {NOTE_E5, 8, 0},
+
+                              {REST, 4, 0},
+                              {NOTE_F5, 3, 0},
+                              {REST, 1, 0},
+                              {NOTE_F5, 4, 0},
+
+                              {REST, 2, 0},
+                              {NOTE_E4, 2, 0},
+                              {NOTE_E5, 8, 0},
+
+                              {NOTE_D5, 2, 0},
+                              {NOTE_F5, 2, 0},
+                              {NOTE_D6, 8, 0},
+
+                              {NOTE_D5, 2, 0},
+                              {NOTE_F5, 2, 0},
+                              {NOTE_D6, 8, 0},
+
+                              {NOTE_E6, 6, 0},
+                              {NOTE_F6, 2, 0},
+                              {NOTE_E6, 2, 0},
+                              {NOTE_F6, 2, 0},
+
+                              {NOTE_E6, 2, 0},
+                              {NOTE_C6, 2, 0},
+                              {NOTE_A5, 8, 0},
+
+                              {NOTE_A5, 4, 0},
+                              {NOTE_D5, 4, 0},
+                              {NOTE_F5, 2, 0},
+                              {NOTE_G5, 2, 0},
+
+                              {NOTE_A5, 11, 0},
+                              {REST, 1, 0},
+
+                              {NOTE_A5, 4, 0},
+                              {NOTE_D5, 4, 0},
+                              {NOTE_F5, 1, 0},
+                              {REST, 1, 0},
+                              {NOTE_F5, 2, 0},
+
+                              {NOTE_E5, 12, 0},
+
+                              {NOTE_D5, 2, 0},
+                              {NOTE_F5, 2, 0},
+                              {NOTE_D6, 8, 0},
+
+                              {NOTE_D5, 2, 0},
+                              {NOTE_F5, 2, 0},
+                              {NOTE_D6, 8, 0},
+
+                              {NOTE_E6, 6, 0},
+                              {NOTE_F6, 2, 0},
+                              {NOTE_E6, 2, 0},
+                              {NOTE_F6, 2, 0},
+
+                              {NOTE_E6, 2, 0},
+                              {NOTE_C6, 2, 0},
+                              {NOTE_A5, 8, 0},
+
+                              {NOTE_A5, 4, 0},
+                              {NOTE_D5, 4, 0},
+                              {NOTE_F5, 2, 0},
+                              {NOTE_G5, 2, 0},
+
+                              {NOTE_A5, 3, 0},
+                              {REST, 1, 0},
+                              {NOTE_A5, 3, 0},
+                              {REST, 1, 0},
+                              {NOTE_A5, 16, 0} //119
     };
 
     //Initialization
@@ -179,7 +268,7 @@ void main(void)
                 sixteenthsPassed = loc_sixteenths;
                 BuzzerOff();
             }
-            if(note >= 50)
+            if(note >= 122)
             {
                 state = LOSE;
             }
