6377dc49f0e85811d53dc64c89a08d8428379199
Nate <nsbajakian@wpi.edu>
2019-02-01 10:08:32 -0500

    Seperated each song into a seperate file

---
+++ gameofthrones.h
@@ -0,0 +1,74 @@
+/*
+ * gameofthrones.h
+ *
+ *  Created on: Feb 1, 2019
+ *      Author: Nathaniel Bajakian
+ */
+#include "notes.h"
+#ifndef GAMEOFTHRONES_H_
+#define GAMEOFTHRONES_H_
+
+
+Note GAMEOFTHRONES[] = {
+
+    {NOTE_G4, 4, 0}, //Patt1
+    {NOTE_C4, 4, 0},
+    {NOTE_Ds4, 2, 0},
+    {NOTE_F4, 2, 0},
+
+    {NOTE_G4, 4, 0}, //Patt1
+    {NOTE_C4, 4, 0},
+    {NOTE_Ds4, 2, 0},
+    {NOTE_F4, 2, 0},
+
+    {NOTE_G4, 4, 0}, //Patt 1
+    {NOTE_C4, 4, 0},
+    {NOTE_Ds4, 2, 0},
+    {NOTE_F4, 2, 0},
+
+    {NOTE_G4, 4, 0}, //Patt 1
+    {NOTE_C4, 4, 0},
+    {NOTE_Ds4, 2, 0},
+    {NOTE_F4, 2, 0},
+
+    {NOTE_G4, 4, 0}, //Patt 2
+    {NOTE_C4, 4, 0},
+    {NOTE_E4, 2, 0},
+    {NOTE_F4, 2, 0},
+
+    {NOTE_G4, 4, 0}, //Patt 2
+    {NOTE_C4, 4, 0},
+    {NOTE_E4, 2, 0},
+    {NOTE_F4, 2, 0},
+
+    {NOTE_G4, 4, 0}, //Patt 2
+    {NOTE_C4, 4, 0},
+    {NOTE_E4, 2, 0},
+    {NOTE_F4, 2, 0},
+
+    {NOTE_G4, 4, 0}, //Patt 2
+    {NOTE_C4, 4, 0},
+    {NOTE_E4, 2, 0},
+    {NOTE_F4, 2, 0},
+
+    {NOTE_G4, 12, 0},
+    {NOTE_C4, 12, 0},
+
+    {NOTE_C4, 1, 0},
+    {NOTE_E4, 2, 0},
+    {NOTE_F4, 2, 0},
+    {NOTE_G4, 8, 0},
+
+    {NOTE_C4, 8, 0},
+    {NOTE_E4, 2, 0},
+    {NOTE_F4, 2, 0},
+
+    {NOTE_B3, 4, 0},
+    {NOTE_D4, 4, 0},
+    {NOTE_G3, 4, 0},
+    {NOTE_B3, 2, 0},
+    {NOTE_C4, 2, 0},
+    };
+
+
+#endif /* GAMEOFTHRONES_H_ */
